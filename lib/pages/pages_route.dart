6ff41782b5fbca6cc08f71a661d0aff44b2e63f3
Neth Wandara <ytdare51@gmail.com>
2023-10-20 16:04:52 +0700

    Login & Sign up UI

--- lib/pages/pages_route.dart
+++ lib/pages/pages_route.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
+import 'package:profiling_app/pages/authenticate/authenticate_view.dart';
 import 'package:profiling_app/pages/home/home_view.dart';
 import 'package:profiling_app/pages/pages_view.dart';
+import 'package:profiling_app/splash_screen.dart';
 
 class PageRouter {
+  static const String splashScreen = '/splash_screen';
   static const String page = '/';
+  static const String authenticate = '/authenticate';
   static const String home = '/home';
   static const String cart = '/cart';
   static const String search = '/search';
@@ -13,8 +17,18 @@ class PageRouter {
 
   static final pages = [
     GetPage(
+      name: splashScreen,
+      page: () => const SplashScreen(),
+      transition: Transition.circularReveal,
+    ),
+    GetPage(
       name: page,
       page: () => const PageView(),
+      transition: Transition.circularReveal,
+    ),
+    GetPage(
+      name: authenticate,
+      page: () => const AuthenticateView(),
     ),
     GetPage(
         name: show,
--- lib/pages/pages_view.dart
+++ lib/pages/pages_view.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:profiling_app/pages/authenticate/authenticate_view.dart';
 import 'package:profiling_app/pages/home/home_controller.dart';
 // import 'package:pizza_food/xcore.dart';
 import 'package:profiling_app/pages/pages_controller.dart';
